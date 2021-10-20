6b6ac5d388555b650ec66825652d4e9a1e582353
Austin Spencer <abspencer2097@gmail.com>
2021-10-19 21:13:12 -0700

    move new task to own page

--- app/imports/ui/components/nav/AddItemBarTop.vue
+++ app/imports/ui/components/nav/AddItemBarTop.vue
@@ -1,54 +1,26 @@
 <template>
   <div v-if="currentUser">
     <div class="top-left">
-      <b-button variant="primary" @click="showModal = !showModal" squared
-        ><i class="fas fa-plus"></i
-      ></b-button>
+      <router-link :to="'/create'" custom v-slot="{ navigate }">
+        <b-button
+          variant="primary"
+          @click="navigate"
+          @keypress.enter="navigate"
+          squared
+          ><i class="fas fa-plus"></i
+        ></b-button>
+      </router-link>
     </div>
-    <b-modal
-      v-model="showModal"
-      title="Add New Task"
-      size="lg"
-      scrollable
-      header-bg-variant="secondary"
-      body-bg-variant="light"
-      body-text-variant="dark"
-      footer-bg-variant="dark"
-      backdrop
-    >
-      <add-item></add-item>
-      <template v-slot:modal-footer="{ close }">
-        <!-- Button with custom close trigger value -->
-        <div class="w-100">
-          <!-- Emulate built in modal footer ok and cancel button actions -->
-          <b-button
-            id="danger"
-            size="md"
-            variant="light"
-            class="float-right"
-            @click="close()"
-          >
-            Cancel
-          </b-button>
-        </div>
-      </template>
-    </b-modal>
   </div>
 </template>
 
 <script>
-import AddItem from "../AddItem.vue";
-
 export default {
   name: "AddItemBar",
-  components: {
-    AddItem,
-  },
+  components: {},
 
   data() {
-    return {
-      showModal: false,
-    };
+    return {};
   },
 
   mounted() {},
