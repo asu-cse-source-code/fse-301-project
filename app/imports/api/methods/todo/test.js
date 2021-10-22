384ca6b59b4c4f43e121f908d75ec7f6a5d15bf1
abspen1 <abspencer2097@gmail.com>
2021-10-22 09:00:53 -0700

    testing

--- app/imports/api/methods/todo/test.js
+++ app/imports/api/methods/todo/test.js
@@ -1,11 +1,16 @@
-const date = new Date();
-const year = date.getFullYear();
-const month = date.getMonth();
-const day = date.getDate();
-const hours = date.getHours();
-const min = date.getMinutes();
-const sec = date.getSeconds();
-const fullTime = `${year}-${month + 1}-${day}T${hours}:${min}:${sec}`;
+// const date = new Date();
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDate();
+// const hours = date.getHours();
+// const min = date.getMinutes();
+// const sec = date.getSeconds();
+// const fullTime = `${year}-${month + 1}-${day}T${hours}:${min}:${sec}`;
 
-console.log(date);
-console.log(fullTime);
+// console.log(date);
+// console.log(fullTime);
+
+const now = new Date();
+const date = new Date(now.getTime() + 180 * 60000);
+
+console.log(now < date);
