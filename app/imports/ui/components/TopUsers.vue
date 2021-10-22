9c520dd2c1588c77603cd525badf7e2010b24af0
abspen1 <abspencer2097@gmail.com>
2021-10-22 10:54:36 -0700

    add limit since not working

--- app/imports/ui/components/TopUsers.vue
+++ app/imports/ui/components/TopUsers.vue
@@ -2,12 +2,14 @@
   <b-container class="mt-5">
     <h3 class="mt-3 mb-3 center"><b>Top Users</b></h3>
     <div v-for="(user, idx) in rewardData" :key="idx">
-      <b-row class="left mb-2">
-        <b-col cols="2"> {{ idx + 1 }}. </b-col>
-        <b-col cols="10">
-          <b>{{ user.username }}</b>
-        </b-col>
-      </b-row>
+      <div v-if="idx < 3">
+        <b-row class="left mb-2">
+          <b-col cols="2"> {{ idx + 1 }}. </b-col>
+          <b-col cols="10">
+            <b>{{ user.username }}</b>
+          </b-col>
+        </b-row>
+      </div>
     </div>
   </b-container>
 </template>
