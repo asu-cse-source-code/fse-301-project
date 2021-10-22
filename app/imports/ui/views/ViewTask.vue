5978fce06927349ac3a052ce450c94f3cd574314
abspen1 <abspencer2097@gmail.com>
2021-10-22 10:55:20 -0700

    add removal to view page

--- app/imports/ui/views/ViewTask.vue
+++ app/imports/ui/views/ViewTask.vue
@@ -13,7 +13,7 @@
     <div class="mt-5" v-else-if="todos && !edit">
       <task-modal :modalData="todos"></task-modal>
     </div>
-    <div class="w-100 mt-4">
+    <div v-if="!removed" class="w-100 mt-4">
       <!-- Emulate built in modal footer ok and cancel button actions -->
       <b-button
         size="md"
@@ -23,6 +23,42 @@
       >
         {{ edit ? "Stop Editing" : "Edit" }}
       </b-button>
+
+      <b-button
+        :variant="`outline-danger`"
+        size="md"
+        class="float-right"
+        square
+        v-b-tooltip.hover
+        title="WARNING: Can't be undone!"
+        @click="removeTask(todos)"
+      >
+        <i class="far fa-trash-alt fa-lg"></i>
+      </b-button>
+    </div>
+    <div v-else>
+      <b-row align-h="center" class="mb-2">
+        <b-col cols="8">
+          <span>Your task was successfully removed!</span>
+        </b-col>
+      </b-row>
+      <b-row align-h="center" class="mt-5">
+        <b-col cols="4">
+          <b-button variant="outline-primary" @click="goToHome()">
+            Home
+          </b-button>
+        </b-col>
+        <b-col cols="4">
+          <b-button variant="outline-primary" @click="goToList()">
+            All Tasks
+          </b-button>
+        </b-col>
+        <b-col cols="4">
+          <b-button variant="outline-primary" @click="goToCreate()">
+            Create Task
+          </b-button>
+        </b-col>
+      </b-row>
     </div>
   </b-container>
 </template>
@@ -43,9 +79,36 @@ export default {
   data() {
     return {
       edit: true,
+      removed: false,
     };
   },
-  methods: {},
+  methods: {
+    removeTask(data) {
+      const self = this;
+
+      Meteor.call("removeTodo", data._id, (error, res) => {
+        if (error) {
+          alert(error.error);
+        } else {
+          if (res !== 1) {
+            alert(`There was an error attempting to remove your task`);
+          } else {
+            self.edit = false;
+            self.removed = true;
+          }
+        }
+      });
+    },
+    goToHome() {
+      this.$router.push({ name: "Home" });
+    },
+    goToCreate() {
+      this.$router.push({ name: "Create" });
+    },
+    goToList() {
+      this.$router.push({ name: "List" });
+    },
+  },
   meteor: {
     $subscribe: {
       todos: [],
